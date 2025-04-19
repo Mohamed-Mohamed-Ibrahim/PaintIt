25f3cea1d875a2e64a758de903f26a69ce5fde18
Mohamed Mohamed Ibrahim <121159820+Mohamed-Mohamed-Ibrahim@users.noreply.github.com>
2025-04-19 11:10:49 +0200

    adding morgan code

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+// const blogRoutes = require("./routes/blogRoutes");
+
+const app = express();
+
+app.listen(8080);
+app.use(morgan("dev"));
+
+// app.use("/blogs", blogRoutes);
