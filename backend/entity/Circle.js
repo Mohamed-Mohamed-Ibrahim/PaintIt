9dbea7d27b337f52a9d9cab81e4d7b0ca70c7799
Mohamed Mohamed Ibrahim <121159820+Mohamed-Mohamed-Ibrahim@users.noreply.github.com>
2025-04-19 15:35:41 +0200

    shape is working

--- backend/entity/Circle.js
+++ backend/entity/Circle.js
@@ -1,17 +1,7 @@
-// const Shape = require("./Shape");
+const Shape = require("./Shape").Shape;
 
 function Circle(data) {
-  this.shape = data.shape;
-  this.x = data.x;
-  this.y = data.y;
-  this.scaleX = data.scaleX;
-  this.scaleY = data.scaleY;
-  this.rotation = data.rotation;
-  this.strokeWidth = data.strokeWidth;
-  this.id = data.id;
-  this.fill = data.fill;
-  this.stroke = data.stroke;
-  this.draggable = data.draggable;
+  this.shape = new Shape(data);
 }
 
 module.exports = {
--- backend/entity/Shape.js
+++ backend/entity/Shape.js
@@ -1,5 +1,15 @@
-class Shape {
-
+function Shape(data) {
+  this.shape = data.shape;
+  this.x = data.x;
+  this.y = data.y;
+  this.scaleX = data.scaleX;
+  this.scaleY = data.scaleY;
+  this.rotation = data.rotation;
+  this.strokeWidth = data.strokeWidth;
+  this.id = data.id;
+  this.fill = data.fill;
+  this.stroke = data.stroke;
+  this.draggable = data.draggable;
 }
 
 module.exports = {
