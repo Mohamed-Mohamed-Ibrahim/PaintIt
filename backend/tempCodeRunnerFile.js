d8cde5c166dee52d963739cb8d7f2db51a021f67
Mohamed Mohamed Ibrahim <121159820+Mohamed-Mohamed-Ibrahim@users.noreply.github.com>
2025-04-22 11:41:05 +0200

    working for circle

--- backend/tempCodeRunnerFile.js
+++ backend/tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-js2xml
+tmp = tools.js2xml({input}, {
+  compact: true});
+console.log(tmp);
+
+console.log(
+  tools.xml2js(tmp, { compact: true })
+);
--- backend/tmp.js
+++ backend/tmp.js
@@ -1,48 +1,47 @@
 const tools = require("xml-js");
-const tools2 = require("simple-xml-to-json");
-
-// const input = [
-//   {
-//     x: 774.8608703613281,
-//     y: 211.43711447632856,
-//     scaleX: 0,
-//     scaleY: 0,
-//     rotation: 0,
-//     strokeWidth: 2,
-//     fill: "white",
-//     stroke: "black",
-//     draggable: false,
-//     radius: 123,
-//     radiusX: 0,
-//     radiusY: 0,
-//     width: 0,
-//     height: 0,
-//     closed: false,
-//     points: null,
-//     shape: "circle",
-//     id: 1,
-//   },
-//   {
-//     x: 1459.8608703613281,
-//     y: 523.4405534976208,
-//     scaleX: 0,
-//     scaleY: 0,
-//     rotation: 0,
-//     strokeWidth: 2,
-//     fill: "white",
-//     stroke: "black",
-//     draggable: false,
-//     radius: 123,
-//     radiusX: 0,
-//     radiusY: 0,
-//     width: 0,
-//     height: 0,
-//     closed: false,
-//     points: null,
-//     shape: "circle",
-//     id: 2,
-//   },
-// ];
+const Shape = require("./model/Shape").Shape
+const input = [
+  {
+    x: 774.8608703613281,
+    y: 211.43711447632856,
+    scaleX: 0,
+    scaleY: 0,
+    rotation: 0,
+    strokeWidth: 2,
+    fill: "white",
+    stroke: "black",
+    draggable: false,
+    radius: 123,
+    radiusX: 0,
+    radiusY: 0,
+    width: 0,
+    height: 0,
+    closed: false,
+    points: null,
+    shape: "circle",
+    id: 1,
+  },
+  {
+    x: 1459.8608703613281,
+    y: 523.4405534976208,
+    scaleX: 0,
+    scaleY: 0,
+    rotation: 0,
+    strokeWidth: 2,
+    fill: "white",
+    stroke: "black",
+    draggable: false,
+    radius: 123,
+    radiusX: 0,
+    radiusY: 0,
+    width: 0,
+    height: 0,
+    closed: false,
+    points: null,
+    shape: "circle",
+    id: 2,
+  },
+];
 // console.log(input);
 // const xml = [];
 // input.forEach((x) =>
@@ -63,32 +62,72 @@ const tools2 = require("simple-xml-to-json");
 // );
 // console.log(json);
 
-const input =   {
-    x: 1459.8608703613281,
-    y: 523.4405534976208,
-    scaleX: 0,
-    scaleY: 0,
-    // rotation: 0,
-    // strokeWidth: 2,
-    // fill: "white",
-    // stroke: "black",
-    // draggable: false,
-    // radius: 123,
-    // radiusX: 0,
-    // radiusY: 0,
-    // width: 0,
-    // height: 0,
-    // closed: false,
-    // points: null,
-    // shape: "circle",
-    // id: 2,
-  };
+// const input = {
+//   x: 1459.8608703613281,
+//   y: 523.4405534976208,
+//   scaleX: 0,
+//   scaleY: 0,
+//   rotation: 0,
+//   strokeWidth: 2,
+//   fill: "white",
+//   stroke: "black",
+//   draggable: false,
+//   radius: 123,
+//   radiusX: 0,
+//   radiusY: 0,
+//   width: 0,
+//   height: 0,
+//   closed: false,
+//   points: null,
+//   shape: "circle",
+//   id: 2,
+// };
 
-let tmp = null;
-tmp = tools.js2xml({input}, {
-  compact: true});
-console.log(tmp);
+let tmp = [];
 
-console.log(
-  tools.xml2js(tmp, { compact: true })
+input.forEach((shape) =>
+  tmp.push(
+    tools.js2xml(
+      { shape },
+      {
+        compact: true,
+      }
+    )
+  )
 );
+
+function cleanShape(shape) {
+  return {
+    x: parseFloat(shape.x._text),
+    y: parseFloat(shape.y._text),
+    scaleX: parseFloat(shape.scaleX._text),
+    scaleY: parseFloat(shape.scaleY._text),
+    rotation: parseFloat(shape.rotation._text),
+    strokeWidth: parseFloat(shape.strokeWidth._text),
+    fill: shape.fill._text,
+    stroke: shape.stroke._text,
+    draggable: shape.draggable._text === "true",
+    radius: parseFloat(shape.radius._text),
+    radiusX: parseFloat(shape.radiusX._text),
+    radiusY: parseFloat(shape.radiusY._text),
+    width: parseFloat(shape.width._text),
+    height: parseFloat(shape.height._text),
+    closed: shape.closed._text === "true",
+    points: null, // original has an empty object
+    shape: shape.shape._text,
+    id: parseInt(shape.id._text),
+  };
+}
+
+
+console.log(tmp);
+let res = []
+tmp.forEach(
+  (shape) => {
+    let ok = new Shape(tools.xml2js(shape, { compact: true }).shape)
+    ok = cleanShape(ok);
+    res.push(ok)
+  }
+)
+
+console.log(res);
